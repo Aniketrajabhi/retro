83c31729d8c928b4d030b2ea03d44f80da844ed2
Aniketrajabhi <aniketrajabhi123@gmail.com>
2023-01-14 22:42:09 +0530

    Initial retrofit commit

--- app/src/main/java/com/aniketto/retrofit/MainActivity.kt
+++ app/src/main/java/com/aniketto/retrofit/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.aniketto.retrofit
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(com.aniketto.retrofit.R.layout.activity_main)
+
+        // hiding SUPPORT BAR and ACTION BAR
+        if (supportActionBar != null) {
+            supportActionBar!!.hide()
+        }
+
+
+        tvLoginlink.setOnClickListener{
+            Intent(this , LoginActivity::class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
